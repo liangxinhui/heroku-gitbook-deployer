2d21a41a29b91e1dae3a37d26e64cbc663b12db1
liangxinhui <liangxinhui@qq.com>
2015-11-09 21:17:09 +0800

    test

--- env_init.sh
+++ env_init.sh
@@ -4,7 +4,7 @@ git config --global user.name "liangxinhui"
 if [ ! -f ~/.ssh/id_rsa ]; then 
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
 fi
-echo 'add this key to github:\n'
+echo '<br>add this key to github:<br>'
 cat ~/.ssh/id_rsa.pub
 
 
@@ -18,21 +18,21 @@ fi
 cd book_src
 
 if [ ! -d EsunnyAPI_FAQ ]; then 
-echo 'Clone EsunnyAPI_FAQ...\n'
+echo '<br>Clone EsunnyAPI_FAQ...<br>'
 git clone https://github.com/liangxinhui/EsunnyAPI_FAQ.git
 fi
 
 # gitbook
 npm install gitbook-cli
 GIBOOK_BIN=${basePath}/node_modules/gitbook-cli/bin/gitbook.js
-unzip -qf ${basePath}/gitbook_versions/2.5.2.zip 
+unzip -f ${basePath}/gitbook_versions/2.5.2.zip 
 mv 2.5.2 ${basePath}/gitbook_versions/2.5.2-liangxh
 if [ ! -d ~/.gitbook/versions/2.5.2-liangxh ];then
-echo 'Link gitbook version...\n'
+echo '<br>Link gitbook version...\n'
 ${GIBOOK_BIN} versions:link ${basePath}/gitbook_versions/2.5.2-liangxh/ 2.5.2-liangxh/
 fi
 
-echo 'Install gitbook plugins...\n'
+echo '<br>Install gitbook plugins...<br>'
 ${GIBOOK_BIN} -v 2.5.2-liangxh install EsunnyAPI_FAQ
 
-echo 'Done'
+echo '<br>Done'
