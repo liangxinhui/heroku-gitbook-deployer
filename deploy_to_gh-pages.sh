cdef015d069dad2ba0ee4c47243124d460eea874
liangxinhui <liangxinhui@qq.com>
2015-11-10 02:12:36 +0000

    test

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -8,25 +8,20 @@ cd ${basePath}/
 if [ ! -d book_src ]; then
 mkdir book_src
 fi
-cd book_src
+cd ${basePath}/book_src
 
 if [ ! -d EsunnyAPI_FAQ ]; then 
 echo '<br>Clone EsunnyAPI_FAQ...<br>'
-git clone https://github.com/liangxinhui/EsunnyAPI_FAQ.git
+git clone git@github.com:liangxinhui/EsunnyAPI_FAQ.git
 fi
 
-
-
-
-cd ${basePath}/book_src
+cd ${basePath}/book_src/EsunnyAPI_FAQ
 rm -rf _book
-
 echo '<p>pull</p>'
 git pull
-GIBOOK_BIN=${basePath}/node_modules/gitbook-cli/bin/gitbook.js
 
+GIBOOK_BIN=${basePath}/node_modules/gitbook-cli/bin/gitbook.js
 echo '<p>build</p>'
-cd ${basePath}/book_src/EsunnyAPI_FAQ
 ${GIBOOK_BIN} -v 2.5.2-liangxh build 
 
 cd _book
