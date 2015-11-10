d83bc692e8e479e108d63cde09836f1d09069490
liangxinhui <liangxinhui@qq.com>
2015-11-10 09:11:19 +0000

    test

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express'); 
 var app = express(); 
-app.get('/', function(req, res){ 
-  res.send('hello world'); 
-  console.log('hello world');
-}); 
+var githubhook = require('express-github-hook');
+
 
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
@@ -14,6 +12,44 @@ function run_cmd(cmd, args, callback) {
   child.stderr.on('data', function(buffer) { console.log(buffer.toString()); resp += buffer.toString(); })
   child.stdout.on('end', function() { callback (resp) });
 }
+
+
+var githubEventFilterOption = {
+    event: 'push',
+    repo: 'EsunnyAPI_FAQ',
+    ref: 'refs/heads/master'
+};
+
+var options = {
+            secret: '123456',
+            treatRequest: function (event, repo, ref, data)
+            {
+                console.log('WEBHOOK ' + event + ' on repository ' + repo + ', ref: ' + ref);
+ 
+                if(githubEventFilterOption.event === "push" && 
+                githubEventFilterOption.repo === repo &&
+                githubEventFilterOption.ref === ref)
+                {
+                    console.log('on event');
+                      run_cmd('sh',[__dirname + "/deploy_to_gh-pages.sh", __dirname],function(data){
+                    	//do nothing
+                      });
+                }
+                else{
+                    console.log('not my event')
+                }
+ 
+            }
+};
+
+app.use('/githubhook', githubhook(options));
+
+app.get('/', function(req, res){ 
+  res.send('hello world'); 
+  console.log('hello world');
+}); 
+
+
 
 app.get('/updatebook', function(req, res){ 
   run_cmd('sh',[__dirname + "/deploy_to_gh-pages.sh", __dirname],function(data){
